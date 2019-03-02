214cdac07511e4f40f44fb5a406c845d085215b6
MariusValentinDirvareanu <dirvareanumarius@gmail.com>
2019-03-02 17:51:20 +0200

    A simple window

---
+++ Tut1/Tut1/Source.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+using namespace std;
+
+const GLint WIDTH = 800, HEIGHT = 600;
+
+
+int main() {
+
+	if (!glfwInit()) {
+		cout << "Initializare GLFW ratata!" << endl;
+		glfwTerminate();
+		return 1;
+	}
+	
+	// Setup GLFW window properties
+	// OpenGL version
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	// Compatibilitate versiune superioara TRUE
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+	GLFWwindow *mainWindow = glfwCreateWindow(WIDTH, HEIGHT, "Test window", NULL, NULL);
+	if (!mainWindow) {
+		cout << "GLFW window creation failed!" << endl;
+		glfwTerminate();
+		return 1;
+	}
+
+	// Get buffer size information
+	int bufferWidth, bufferHeight;
+	glfwGetFramebufferSize(mainWindow, &bufferWidth, &bufferHeight);
+
+	// Set the context for GLEW to use
+	glfwMakeContextCurrent(mainWindow);
+
+	// Allow modern extension features
+	glewExperimental = GL_TRUE;
+
+	if (glewInit() != GLEW_OK) {
+		cout << "GLEW init failed!" << endl;
+		glfwDestroyWindow(mainWindow);
+		glfwTerminate();
+		return 1;
+	}
+
+	// Setup viewport size
+	glViewport(0, 0, bufferWidth, bufferHeight);
+
+	// Loop until window close
+	while (!glfwWindowShouldClose(mainWindow)) {
+		// Get and handle user input events
+		glfwPollEvents();
+
+		// Clear the window
+		glClearColor(0.0f, 1.0f, 0.0f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glfwSwapBuffers(mainWindow);
+	}
+
+	return 0;
+}
